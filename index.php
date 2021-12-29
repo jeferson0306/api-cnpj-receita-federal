35ab19fa3a1dea07a3bfafcddcd25e84bcbb302e
Jeferson Siqueira <73137639+jeferson0306@users.noreply.github.com>
2021-12-29 01:18:17 -0300

    API consulta CNPJ na Receita Federal

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API CNPJ - Receita Federal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="row">
+    <div class="col-md-4">
+
+    </div>
+
+    <div class="col-md-4">
+        <div class="form-group row">
+            <div class="col-md-12"><br><br>
+                <label for="" >CNPJ</label>
+                <input type="text" onblur="checkCnpj(this.value)" data-mask="00.000.000/0000-00" class="form-control">
+            </div>          
+        </div>
+
+        <div class="form-group row">
+            <div class="col-md-6">
+                <label for="" id="razaosocial">Razão Social</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="col-md-6">
+                <label for="" id="fantasia">Nome Fantasia</label>
+                <input type="text" class="form-control">
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-md-6">
+                <label for="" id="logradouro">Logradouro</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="col-md-6">
+                <label for="" id="numero">Número</label>
+                <input type="text" class="form-control">
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-md-6">
+                <label for="" id="municipio">Município</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="col-md-6">
+                <label for="" id="uf">UF</label>
+                <input type="text" class="form-control">
+            </div>
+        </div>
+    </div>
+        
+
+    <div class="col-md-4">
+        
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js"></script>
+
+    <script>
+        //script funciona apenas no console, não está retornando nos inputs
+        function checkCnpj(cnpj) {
+            $.ajax({
+                'url': 'https://www.receitaws.com.br/v1/cnpj/' +  cnpj.replace(/[^0-9]/g, ''),
+                'type': "GET",
+                'dataType': 'jsonp',
+                'success': function(data) {
+                    if(data.nome == undefined) {
+                        alert(data.status + ' ' + data.message)
+                    } else {
+                        document.getElementById('razaosocial').value = data.nome;
+                        document.getElementById('fantasia').value = data.fantasia;
+                        document.getElementById('logradouro').value = data.logradouro;
+                        document.getElementById('numero').value = data.numero;
+                        document.getElementById('municipio').value = data.municipio;
+                        document.getElementById('uf').value = data.uf;
+                    }
+                    console.log(data);
+                }
+            })
+        }
+    </script>
+
+</body>
+</html>
